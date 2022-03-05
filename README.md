3255bcfc6bb6f335b209945d440d4115445fc968
boburjon09012001 <bobirislomjonov@gmail.com>
2022-03-06 03:55:20 +0500

    screen images upload

--- README.md
+++ README.md
@@ -3,10 +3,4 @@
 A new Flutter project.
 
  ![](assets/screens/head1.png)
- ![](assets/screens/head2.png)
- ![](assets/screens/head3.png)
- ![](assets/screens/head4.png)
- ![](assets/screens/head5.png)
- ![](assets/screens/head6.png)
- ![](assets/screens/head7.png)
- ![](assets/screens/head8.png)
+
--- lib/pages/food_recipe.dart
+++ lib/pages/food_recipe.dart
@@ -16,4 +16,13 @@ class Food{
     Food("assets/images/foodbig.png", "Beef Burger", "Mcdi", "Rp. 20.000"),
   ];
 
+
+
+  // ![](assets/screens/head2.png),
+  // ![](assets/screens/head3.png),
+  // ![](assets/screens/head4.png),
+  // ![](assets/screens/head5.png),
+  // ![](assets/screens/head6.png),
+  // ![](assets/screens/head7.png),
+  // ![](assets/screens/head8.png),
 }
